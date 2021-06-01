1ab0ee3c5290e46c0a7ded7c93350ecbf31454af
ioanatatu <tatu.ioana@gmail.com>
2021-06-01 22:55:57 +0200

    add documentation and propTypes to Metadata component

--- src/components/Metadata/Metadata.js
+++ src/components/Metadata/Metadata.js
@@ -1,27 +1,35 @@
-// style
 import css from './Metadata.module.css';
+
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Metadata = ({ currentWord }) => {
-   // useEffect(() => {
-   //    console.log('###', currentWord);
-   // }, [currentWord]);
+/**
+ * Metadata component which offers additional information about the clicked word
+ *
+ * @param {string} label The topic that has been clicked in the word cloud.
+ * @param {number} volume Total number of mentions.
+ * @param {object} sentiment Object with sentiment breakdown.
+ */
+const Metadata = ({
+   label,
+   volume,
+   sentiment: { positive = 0, negative = 0, neutral = 0 },
+}) => (
+   <div className={css.general}>
+      <h2>{label}</h2>
+      <h3>Total Mentions: {volume}</h3>
+      <ul className={css.detailedMentions}>
+         <li>Positive: {positive}</li>
+         <li>Neutral: {neutral}</li>
+         <li>Negative: {negative}</li>
+      </ul>
+   </div>
+);
 
-   return (
-      <div className={css.general}>
-         {currentWord && currentWord.sentiment && (
-            <>
-               <h2>{currentWord.label}</h2>
-               <h3>Total Mentions: {currentWord.volume}</h3>
-               <ul className={css.detailedMentions}>
-                  <li>Positive: {currentWord.sentiment.positive || 0}</li>
-                  <li>Neutral: {currentWord.sentiment.neutral || 0}</li>
-                  <li>Negative: {currentWord.sentiment.negative || 0}</li>
-               </ul>
-            </>
-         )}
-      </div>
-   );
+Metadata.propTypes = {
+   label: PropTypes.string,
+   volume: PropTypes.number,
+   sentiment: PropTypes.object,
 };
 
 export default Metadata;
